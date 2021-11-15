bb25d818f0ab2e24a2aea77a1aa08cacc07dd9d4
AndreaLK3 <andrea.lekkas@outlook.com>
2021-11-15 11:49:56 +0100

    =...

--- ExploreDataset.py
+++ ExploreDataset.py
@@ -6,6 +6,7 @@
 # - Average number of words in an article, per class; bar plot.
 #   This visualization and the previous one are already found on the site
 # - Numerical table or color-matrix, expressing the overlap in vocabulary between any 2 classes
+# - Bar plot: how much of the vocabulary of a class is non-overlapping?
 
 import Utils
 import matplotlib.pyplot as plt
@@ -13,18 +14,20 @@ from sklearn.feature_extraction.text import CountVectorizer
 import nltk
 import numpy as np
 
-def num_articles():
-    training_df = Utils.load_split(Utils.Split.TRAINING)
-    num_articles_per_class_trainingdf = training_df.groupby(Utils.Column.CLASS.value).count()
-    # print(str(num_articles_per_class_trainingdf))
-    test_df = Utils.load_split(Utils.Split.TEST)
 
+def initialize():
+    training_df = Utils.load_split(Utils.Split.TRAINING)
     class_frequencies_training = list(training_df["class"].value_counts())
-    class_frequencies_test = list(test_df["class"].value_counts())
-    class_frequencies_total = [class_frequencies_training[i] + class_frequencies_test[i]
-                               for i in range(len(class_frequencies_training))]
     class_names = list(training_df["class"].value_counts().index)
-    # print(str(class_frequencies_training))
+
+    # test_df = Utils.load_split(Utils.Split.TEST)
+    # class_frequencies_test = list(test_df["class"].value_counts())
+    # class_frequencies_total = [class_frequencies_training[i] + class_frequencies_test[i]
+    #                           for i in range(len(class_frequencies_training))]
+
+    return training_df, class_frequencies_training, class_names
+
+def num_articles(class_frequencies_training, class_names):
 
     fig, ax = plt.subplots()
     xlocs, xlabs = plt.xticks()
@@ -38,14 +41,7 @@ def num_articles():
     # plt.legend(["Training set", "Test set"])
     plt.show()
 
-def words_in_articles():
-    # For each class, we must reunite the text of the articles, and count the number of tokens.
-    # This is viable because the dataset is very small, otherwise we would have to split it and use partial results
-    training_df = Utils.load_split(Utils.Split.TRAINING)
-    # test_df = Utils.load_split(Utils.Split.TEST)
-
-    training_articles = training_df[Utils.Column.ARTICLE.value].to_list()
-    # test_articles = test_df[Utils.Column.ARTICLE.value].to_list()
+def words_in_articles(training_df):
 
     class_names = list(training_df["class"].value_counts().index)
     avg_words_per_class = []
@@ -60,7 +56,7 @@ def words_in_articles():
     xlocs, xlabs = plt.xticks()
     bar_obj = plt.bar(x=class_names, height=avg_words_per_class, width=0.6, color="y")
     plt.xticks(rotation=45)
-    plt.xlabel('Classes')
+    plt.xlabel('Class')
     plt.ylabel('Words')
     plt.title("Average words per article - training set")
     plt.bar_label(bar_obj, labels=avg_words_per_class)
@@ -69,25 +65,20 @@ def words_in_articles():
 
     return vocabularies_ls
 
-def vocabulary_of_classes():
-    # For each class, we must reunite the text of the articles, and count the number of tokens.
-    # This is viable because the dataset is very small, otherwise we would have to split it and use partial results
-    # training_df = Utils.load_split(Utils.Split.TRAINING)
-    test_df = Utils.load_split(Utils.Split.TEST)
-
-    # training_articles = training_df[Utils.Column.ARTICLE.value].to_list()
-    test_articles = test_df[Utils.Column.ARTICLE.value].to_list()
-
-    class_names = list(test_df["class"].value_counts().index)
+def get_class_vocabularies(training_df, class_names):
     num_classes = len(class_names)
     classes_words_ls = []
     for c_name in class_names:
-        class_articles = test_df[test_df[Utils.Column.CLASS.value] == c_name][
+        class_articles = training_df[training_df[Utils.Column.CLASS.value] == c_name][
             Utils.Column.ARTICLE.value].to_list()
         vectorizer = CountVectorizer(lowercase=False)
         vectorizer.fit(class_articles)
         words_in_class = set(vectorizer.vocabulary_.keys())
         classes_words_ls.append(words_in_class)
+    return classes_words_ls
+
+def vocabulary_overlap(class_names, classes_words_ls):
+    num_classes = len(class_names)
 
     overlap_matrix = np.zeros((num_classes,num_classes))
 
@@ -104,8 +95,6 @@ def vocabulary_of_classes():
                   "; |" + str(class_names[j]) + "|= " + str(len(words_class_j))
             + "; |overlap|= " + str(cardinality_overlap_i_j) + "; overlap=" + str(round(overlap_i_j,2)))
 
-
-
     fig, ax = plt.subplots()
     ax.set_xticklabels(class_names)
     ax.set_yticklabels(class_names)
@@ -116,3 +105,29 @@ def vocabulary_of_classes():
     plt.title("Overlap in vocabulary between classes")
 
     return overlap_matrix
+
+def vocabulary_unique(class_names, classes_words_ls):
+
+    num_classes = len(class_names)
+    unique_vocabulary_fraction_ls = []
+    for i in range(num_classes):
+        vocab_class_i = classes_words_ls[i]
+        cardinality_vocab_class_i = len(vocab_class_i)
+        for j in range(num_classes):
+            if j != i:
+                vocab_class_j = classes_words_ls[j]
+                vocab_class_i = vocab_class_i.difference(vocab_class_j)
+        unique_vocabulary_fraction = round(len(vocab_class_i) / cardinality_vocab_class_i,3)
+        unique_vocabulary_fraction_ls.append(unique_vocabulary_fraction)
+    bar_obj = plt.bar(x=class_names, height=unique_vocabulary_fraction_ls, width=0.6, color="g")
+    plt.xticks(rotation=45)
+    plt.xlabel('Class')
+    plt.ylabel('Percentage')
+    plt.title("% of vocabulary unique to the class")
+    plt.grid(color='lightgray', linestyle='-', linewidth=0.2, zorder=-1)
+    plt.bar_label(bar_obj, labels=unique_vocabulary_fraction_ls, zorder=5)
+    # plt.legend(["Training set", "Test set"])
+    plt.show()
+
+
+
